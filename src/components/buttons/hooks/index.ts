61df895975f274044c901ec7d34f8ce37f675219
geprojetos <geprojetosbrasil@gmail.com>
2022-10-14 20:50:01 -0300

    refactor useButtons

--- src/components/buttons/hooks/index.ts
+++ src/components/buttons/hooks/index.ts
@@ -1,18 +1,3 @@
-import { useState } from "react";
-
-const useButtons = () => {
-  const [counter, setCounter] = useState(0);
-
-  const buttons = {
-    handleClickIncrement: () => {
-      setCounter((oldState) => oldState + 1);
-    },
-    handleClickDecrement: () => {
-      setCounter((oldState) => oldState - 1);
-    },
-  };
-
-  return { counter, buttons };
-};
+import { useButtons } from "./useButtons";
 
 export { useButtons };
